ae931662003a31fd6bb5c232ee6a5b6774c8ab06
Samar Pratap Singh <57188508+samar-1601@users.noreply.github.com>
2020-07-05 21:09:06 +0530

    Files added to the repository

---
+++ index.js
@@ -0,0 +1,85 @@
+update();// Updates the table with whatever the initial value is there in the localstorage;
+
+let add = document.getElementById("add");
+
+add.addEventListener("click",getAndUpdate);
+
+
+function getAndUpdate()
+{
+    console.log('adding');
+    title = document.getElementById('title').value;
+    description = document.getElementById('description').value;
+    if(localStorage.getItem('itemsJson') == null && title !=null)
+    {
+        taskJsonArray = [];
+        taskJsonArray.push([title,description]);
+        localStorage.setItem('itemsJson',JSON.stringify(taskJsonArray));
+    }
+    else if(title != null){
+        taskJsonArrayString = localStorage.getItem('itemsJson');
+        taskJsonArray = JSON.parse(taskJsonArrayString);
+        taskJsonArray.push([title,description]);
+        localStorage.setItem('itemsJson',JSON.stringify(taskJsonArray));
+    }
+    
+    update();
+}
+function update()
+{
+    if(localStorage.getItem('itemsJson') == null && title !=null)
+    {
+        taskJsonArray = [];
+        localStorage.setItem('itemsJson',JSON.stringify(taskJsonArray));
+    }
+    else {
+        taskJsonArrayString = localStorage.getItem('itemsJson');
+        taskJsonArray = JSON.parse(taskJsonArrayString);
+        localStorage.setItem('itemsJson',JSON.stringify(taskJsonArray));
+    }
+    document.getElementById("title").value = "";
+    document.getElementById("description").value = "";
+// Code for Entering the user entries in th table
+let tableBody = document.getElementById('tableBody');
+dataString = '';
+taskJsonArray.forEach((element,index) => {
+    if(element[0] == '')
+    {   
+        continue;
+    }
+    else
+    dataString += `
+    <tr>
+        <th scope="row">${index +1 }</th>
+        <td>${element[0]}</td>
+        <td>${element[1]}</td>
+        <td><button class="btn btn-sm btn-primary" onclick = "deleteEntry(${index})">Delete</button></td>
+    </tr>
+    `    
+});
+
+tableBody.innerHTML = dataString;
+
+}
+
+function deleteEntry(itemIndex)
+{
+    if(confirm("Have you really completed this task and want to delete it ?"))
+    {
+        console.log('Deleting');
+        taskJsonArrayString = localStorage.getItem('itemsJson');
+        taskJsonArray = JSON.parse(taskJsonArrayString);
+        taskJsonArray.splice(itemIndex,1);
+        localStorage.setItem('itemsJson',JSON.stringify(taskJsonArray));
+        update();
+    }
+}
+
+function clearList()
+{
+    if(confirm("Do you want to clear the entire list ?"))
+    {
+        localStorage.clear();
+        update();
+    }
+}
